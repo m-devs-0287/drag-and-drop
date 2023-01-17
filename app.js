8221b649b1824c63f6008c26a2d3f0ecdf03be0c
Manuel D. Nunez <113308860+m-devs-0287@users.noreply.github.com>
2023-01-16 23:24:13 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+const fill = document.querySelector('.fill')
+const empties = document.querySelectorAll('.empty')
+
+fill.addEventListener('dragstart', dragStart)
+fill.addEventListener('dragend', dragEnd)
+
+for(const empty of empties){
+  empty.addEventListener('dragover', dragOver)
+  empty.addEventListener('dragenter', dragEnter)
+  empty.addEventListener('dragleave', dragLeave)
+  empty.addEventListener('drop', dragDrop)
+}
+
+function dragStart() {
+
+  this.className += ' hold'
+  setTimeout(() => this.className += ' invisible', 0)
+    
+  
+
+  console.log("drag Start");
+
+}
+
+function dragEnd() {
+  console.log("drag End");
+}
+function dragOver(e) {
+  e.preventDefault();
+  
+  console.log("drag Over");
+}
+function dragEnter(e) {
+  e.preventDefault();
+  
+  console.log("drag Enter");
+}
+
+function dragLeave() {
+  console.log("drag Leave");
+}
+function dragDrop() {
+  this.className = 'empty'
+  
+  
+  console.log(this);
+  this.append(fill)
+}
